ff106a83f62bc8a3cb1fe7d85a572a626371d5c6
Wade Green <61675550+TrialAndErrror@users.noreply.github.com>
2022-07-08 09:22:32 -0400

    initial commit

---
+++ main.py
@@ -0,0 +1,5 @@
+from src.docs import print_docs
+
+if __name__ == '__main__':
+    print_docs()
+
---
+++ src/docs.py
@@ -0,0 +1,5 @@
+
+def print_docs():
+    print('PyTermGUI Repo located at https://github.com/bczsalba/pytermgui')
+    print('Docs located at https://ptg.bczsalba.com/pytermgui.html')
+    print('Learn about using YAML in Python at https://realpython.com/python-yaml/')
